ebfc9e645fbbac6e98ee96f59692061c66659682
Felipe Souza <fn23886@gmail.com>
2024-09-05 09:37:03 -0300

    feat : alterações gerais nos produtos

--- project_react/src/components/product-card/product-card.jsx
+++ project_react/src/components/product-card/product-card.jsx
@@ -1,54 +1,28 @@
 import "./styles.css";
 
-export const ProductCard =({imgUrl,nomeProduto,tamanhos,cores,preco,}) =>{
+export const ProductCard =({imgUrl,nomeProduto,preco,}) =>{
     return (
 		<li>
 			<div className="product_wrap">
 				<div className="product_item">
+					
 					<div className="img">
 						<img src={imgUrl} alt="Round_neck" />
 						<p className="brand_name">{nomeProduto}</p>
-					</div>
-					<div className="size_color">
-						<div className="title">SIZE & COLOR</div>
-						<div className="size_wrap">
-							{tamanhos}
-							{/* <ul>
-								<li>
-									<span>36</span>
-								</li>
-								<li>
-									<span>38</span>
-								</li>
-								<li>
-									<span>40</span>
-								</li>
-								<li>
-									<span>42</span>
-								</li>
-							</ul> */}
-						</div>
-						<div className="color_wrap">
-							<ul>
-								<li
-									data-color={cores}
-									data-src="https://i.imgur.com/a31ZhsD.png"
-								></li>
-								<li
-									className="active"
-									data-color={cores}
-									data-src="https://i.imgur.com/vuTSHe3.png"
-								></li>
-								<li
-									data-color={cores}
-									data-src="https://i.imgur.com/TT6hsaQ.png"
-								></li>
-							</ul>
-						</div>
-					</div>
+					</div><br></br>
+
 					<div className="price">
 						<p>{preco}</p>
 					</div>
+
+					<div className="size_color">
+						<div className="title">Linha Exclusiva</div>
+						
+						<div>
+							<button className="compra_btn">Compre Agora &#128722;</button>	
+						</div>
+					</div>
+					
 				</div>
 			</div>
 		</li>
--- project_react/src/components/product-list/product-list.jsx
+++ project_react/src/components/product-list/product-list.jsx
@@ -1,37 +1,50 @@
 import { ProductCard } from "../product-card/product-card"
+import "./styles.css"
+
+// vou adicionar uma array que deixara a lista de produtos mais dinâmica 
+
+const listaDEProdutos = [
+    {
+        nomeProduto:"Malbac Tradicional",
+        preco: "R$ 300,00",
+        imgUrl: "https://res.cloudinary.com/beleza-na-web/image/upload/w_1500,f_auto,fl_progressive,q_auto:eco,w_210,h_210/v1/imagens/products/B48130/B48130_Malbec_Desodorante_Col%C3%B4nia_1.jpg",
+    },
+    {
+        nomeProduto: "Malbac Bluer",
+        preco: "R$ 299,35",
+        imgUrl: "https://res.cloudinary.com/beleza-na-web/image/upload/w_1500,f_auto,fl_progressive,q_auto:eco,w_800/v1/imagens/products/B83188/B83188_Malbec-Bleu-Desodorante-Colonia-100ml_.jpg",
+    },
+    {
+        nomeProduto: "Malbac Ultra Bluer",
+        preco: "R$ 250,40",
+        imgUrl: "https://res.cloudinary.com/beleza-na-web/image/upload/w_1500,f_auto,fl_progressive,q_auto:eco,w_210,h_210/v1/imagens/product/B50119/4e3b5189-ef7c-4f03-8c69-ea5ce169bc48-bot-50119-malbec-ultra-bleu-frontal-01.jpg",
+    },
+    {
+        nomeProduto: "Malbac Icon",
+        preco: "R$ 370,54",
+        imgUrl: "https://res.cloudinary.com/beleza-na-web/image/upload/w_1500,f_auto,fl_progressive,q_auto:eco,w_210,h_210/v1/imagens/product/B54150/08d16f28-be2f-4c3d-936a-28853771a540-bot-54150-malbec-icon-colonia-frontal-01.jpg",
+    }
+]
+
 
 export const ProductList = () => {
     return (
-        <ul>
-            <h1>ProductList</h1>
-            <ProductCard 
-                cores={"#fff"}
-                imgUrl = {"https://media.istockphoto.com/id/1132154377/pt/foto/jeans.jpg?s=1024x1024&w=is&k=20&c=vQ-o-eI1mJFcxpSm7POj-4RBLNyT7YC-_hwSz73wv7g="}
-                preco = {"R$ 23"}
-                nomeProduto= {"calça"}
-                tamanhos={12}
-            />
-            <ProductCard 
-                cores={"#fff"}
-                imgUrl = {"../src/assets/clohtes/—Pngtree—white polo shirt mockup realistic_5268053.png"}
-                preco = {"R$ 23"}
-                nomeProduto= {"camisa"}
-                tamanhos={12}
-            />
-            <ProductCard 
-                cores={"#fff"}
-                imgUrl = {"../src/assets/clohtes/png-transparent-jacket-duffel-coat-hood-blue-jacket.png"}
-                preco = {"R$ 15"}
-                nomeProduto= {"casaco de pobre"}
-                tamanhos={15} 
-            />
-            <ProductCard 
-              cores={"#fff,red"}
-              imgUrl = {"../src/assets/clohtes/kisspng-down-feather-jacket-down-jacket-5a9752f79ce596.2586811915198666156427.jpg"}
-              preco = {"R$ 30"}
-              nomeProduto= {"casaco de rico"}
-              tamanhos={15} 
-            />
+       <>
+       <div className="conteiner_title">
+        <h1 className="title">Produtos a Venda</h1>
+       </div>
+        <ul className="product-list-container">
+            {listaDEProdutos.map((item,index) => (
+                <ProductCard
+                    key={index} //Parametro usado para renderizar listas em react, para mapear melhor
+                    nomeProduto={item.nomeProduto}
+                    preco={item.preco}
+                    imgUrl={item.imgUrl} 
+                />
+            ))}
         </ul>
+       </>
     )
 }
+
+// O map vai retornar um novo array com os objetos dentro do ProductCard --> <ProductCard />, <ProductCard />, <ProductCard />, <ProductCard />]
